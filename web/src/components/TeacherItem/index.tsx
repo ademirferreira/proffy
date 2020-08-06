b0a42e014a858fa685d8308f10d0bf44f481fd8d
Ademir Ferreira <whoisademir@gmail.com>
2020-08-05 21:45:18 -0300

    Add feature to list teachers

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -4,32 +4,50 @@ import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './style.css'
 
-function TeacherItem() {
+import api from '../../services/api';
+
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+interface TeacherItemProps {
+  teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+
+  function createConnection() {
+    api.post('connections', {
+      user_id: teacher.id
+    })
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://avatars2.githubusercontent.com/u/13952497?s=460&u=f7537a6a4e1b522eaa266648d3a64ccc9b707938&v=4" alt="Ademir Ferreira" />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Ademir Ferreira</strong>
-          <span>Matemática</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Mussum Ipsum, cacilds vidis litro abertis.
-            <br /><br />
-            Mussum Ipsum, cacilds vidis litro abertis. Casamentiss faiz malandris se pirulitá. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.
-          </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/Hora
-              <strong>R$ 80,00</strong>
+              <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a onClick={createConnection} href={`https://wa.me/${teacher.whatsapp}`} target="blank">
           <img src={whatsappIcon} alt="whatsapp" />
               Entrar em contato
-            </button>
+        </a>
       </footer>
     </article>
   )
